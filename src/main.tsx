fd108a41e480e7ebbcf8db6722686c850c111847
Yarik <57310769+DeFosu@users.noreply.github.com>
2025-04-03 11:34:12 +0300

    Organize the import of the typography component

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Typography } from "./components/Typography";
 import "./index.css";
+import { Typography } from "./components/Typography";
 
 const App = () => {
   return (
